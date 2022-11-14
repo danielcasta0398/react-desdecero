65e68ec5da9641a37ae53590f03f73cb4e108e20
unknown <juancastano0398@gmail.com>
2022-11-14 22:25:00 +0100

    deploy

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import { Contacto } from '../components/Contacto/Contacto'
 import { Home } from '../components/Home/Home'
 
 export const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path='/home' element={<Home />} />
             <Route path='/contacto' element={<Contacto />} />
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
